727c201276c236cb9163b1a3fbd108cd287b2eb3
makarim muhammad <jakamakarim@gmail.com>
2026-01-22 18:00:58 +0700

    feat : add about and home

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 function About() {
   return (
-    <div>
-      <h1>Tentang Kami</h1>
-      <p>Kami adalah tim yang bersemangat untuk membuat hal-hal hebat.</p>
+    <div className="flex flex-row">
+      <div className="flex flex-col justify-around font-sans text-xl p-15">
+        <p>Our <strong className="text-2xl text-blue-500">founder</strong> </p>
+        <p>Meet Makarim Muhammad, the mastermind behind this successful project that turns billions</p>
+      </div>
+      <div className="flex flex-col justify-around font-sans text-xl p-15">
+        <img className='rounded-full w-75 h-75 ' src="src/assets/photome.jpg" alt="image" />
+      </div>
     </div>
   );
 }

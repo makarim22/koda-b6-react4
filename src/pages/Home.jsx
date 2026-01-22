674519259b5b412157410a2ca60fffddc027ea29
makarim muhammad <jakamakarim@gmail.com>
2026-01-22 17:48:48 +0700

    feat : insert image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 
 function Home() {
   return (
-    <div>
-      <h1>Selamat Datang di Halaman Utama!</h1>
-      <p>Ini adalah halaman beranda Anda.</p>
+    <div className="flex flex-row">
+        <div className="flex flex-col justify-around font-sans text-xl p-15">
+      <p>The <strong className="text-2xl text-blue-500">easiest way</strong> to get your new job</p>
+      <p>Each month more than 3 million job seekers turn to website in their search for work, making over 140.00 applications every single day</p>
+        </div>
+<div>
+    <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="image" />
+</div>
     </div>
+
   );
 }
 
